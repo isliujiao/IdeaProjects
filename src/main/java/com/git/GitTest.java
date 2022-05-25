8b80f5c418800b521e62d86280b66ce91f113013
isliujiao <isliujiao@163.com>
2022-05-25 08:49:40 +0800

    three commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -4,5 +4,7 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("********* add_v1");
+        System.out.println("********* add_v2");
+        System.out.println("********* add_v3");
     }
 }
