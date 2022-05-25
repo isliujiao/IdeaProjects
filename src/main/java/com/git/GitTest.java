a505fd831526b947a3a1024f5a69b1fa41ca4dae
isliujiao <isliujiao@163.com>
2022-05-25 14:19:38 +0800

    push commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -1,10 +1,11 @@
 package com.git;
-
-
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("********* add_v1");
         System.out.println("********* add_v2");
         System.out.println("********* add_v3");
+        System.out.println("********* branch_v1");
+        System.out.println("********* push_v1");
+
     }
 }
